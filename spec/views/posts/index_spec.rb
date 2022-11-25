93390cdea2a072caa474d6e5f73f8c47c0526dcf
Pierre <piercel.coder2022@gmail.com>
2022-11-25 12:15:37 +0100

    Add test for post index

--- spec/views/posts/index_spec.rb
+++ spec/views/posts/index_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe 'Post index page', type: :feature do
   before(:example) do
     @user = User.create(name: 'Elon Musk', photo: 'link/goes/here', bio: 'this is my bio')
@@ -10,39 +9,30 @@ RSpec.describe 'Post index page', type: :feature do
     Comment.create(text: 'My first awesome comment straight from Kenya', author_id: @user.id, post_id: @post_one.id)
     visit user_posts_path(@user)
   end
-
   it 'renders photo of the user' do
     expect(page.html).to include(@user.photo)
   end
-
-  it 'shows username of a given user' do
+  it 'show username of a given user' do
     expect(page).to have_content('Elon Musk')
   end
-
   it 'shows number of posts written by agiven user' do
     expect(page).to have_content(4)
   end
-
   it 'shows post title of the user' do
     expect(page).to have_content('My first post')
   end
-
   it 'shows first comments of the post' do
     expect(page).to have_content('My first awesome comment straight from Kenya')
   end
-
   it 'shows how many comments a post has' do
     expect(page).to have_content(1)
   end
-
   it 'shows how many likes a post has' do
     expect(page).to have_content(0)
   end
-
   it 'shows a section for pagination if there are more posts' do
     expect(page.html).to have_content('Pagination')
   end
-
   it 'redirects to a given user show page' do
     click_link @post_one.title
     expect(page).to have_current_path(user_post_path(@user.id, @post_one))
--- spec/views/users/show_spec.rb
+++ spec/views/users/show_spec.rb
@@ -1,30 +1,39 @@
 require 'rails_helper'
-
-RSpec.describe 'User post', type: :feature do
+RSpec.describe 'User show', type: :feature do
   before(:example) do
     @user = User.create(name: 'Elon Musk', photo: 'link/goes/here', bio: 'this is my bio')
-    @post_one = Post.create(title: 'My first post', text: 'Body text of the post', author_id: @user.id)
+    Post.create(title: 'My first post', text: 'Body text of the post', author_id: @user.id)
     Post.create(title: 'My second post', text: 'Body text of the post', author_id: @user.id)
     Post.create(title: 'My third post', text: 'Body text of the post', author_id: @user.id)
     Post.create(title: 'My fourth post', text: 'Body text of the post', author_id: @user.id)
-    Comment.create(text: 'My first awesome comment straight from Kenya', author_id: @user.id, post_id: @post_one.id)
-
-    visit user_post_path(@user, @post_one.id)
+    visit user_path(@user)
   end
-
-  it 'renders who wrote the post ' do
-    expect(page.html).to include(@user.name)
+  it 'renders photo of the user' do
+    expect(page.html).to include(@user.photo)
   end
-
-  it 'renders number of comments' do
-    expect(page).to have_content(1)
+  it 'show username of a given user' do
+    expect(page).to have_content('Elon Musk')
   end
-
-  it 'renders number of likes ' do
+  it 'shows number of posts written by agiven user' do
     expect(page).to have_content(0)
   end
-
-  it 'renders post body ' do
-    expect(page).to have_content(@post_one.text)
+  it 'shows bio of the user' do
+    expect(page).to have_content('this is my bio')
+  end
+  it 'shows the first three posts of the user' do
+    expect(page).to have_content('My fourth post')
+    expect(page).to have_content('My third post')
+    expect(page).to have_content('My second post')
+  end
+  it 'shows the see all posts button' do
+    expect(page.html).to have_content('See all posts')
+  end
+  it 'redirects to a given user show page' do
+    click_link @user.name
+    expect(page).to have_current_path(user_path(@user.id))
+  end
+  it "redirects to a user's posts index" do
+    click_link 'See all posts'
+    expect(page).to have_current_path(user_posts_path(@user.id))
   end
 end
